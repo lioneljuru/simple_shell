42f652db558658d7b3dd09391cf1219415511b19
jumacol <jurumacol@gmail.com>
2023-11-12 21:15:45 +0200

    simp

---
+++ shell_main.c
@@ -0,0 +1,52 @@
+#include "shell.h"
+
+char **commands = NULL;
+char *line = NULL;
+char *shell_name = NULL;
+int status = 0;
+
+/**
+ * main - the mainshell code
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	char **current_command = NULL;
+	int i, type_command = 0;
+	size_t n = 0;
+
+	signal(SIGINT,ctrl_c_handler);
+	shell_name = argv[0];
+	while (1)
+	{
+		non_interactive();
+		print("($) ", STDOUT_FILENO);
+		if (getline(&line, &n, stdin) == -1)
+		{
+			free(line);
+			exit(status);
+		}
+		remove_newline(line);
+		remove_comment(line);
+		commands = tokenizer(line, ";");
+
+		for (i = 0; commands[i] != NULL; i++)
+		{
+			current_command = tokenizer(commands[i], " ");
+			if (current_command[0] == NULL)
+			{
+				free(current_command);
+				break;
+			}
+			type_command = parse_command(current_command[0]);
+			initializer(current_command, type_command);
+			free(current_command);
+		}
+		free(commands);
+	}
+	free(line);
+	return (status);
+}
---
+++ strings.c
@@ -0,0 +1,103 @@
+#include "shell.h"
+/**
+ * _strcmp - compares two strings
+ * @first: first string to compare
+ * @second: second string
+ *
+ * Return: difference between strings
+ */
+int _strcmp(char *first, char *second)
+{
+	int i = 0;
+
+	while (first[i] != '\0')
+	{
+		if (first[i] != second[i])
+			break;
+		i++;
+	}
+	return (first[i] - second[i]);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @destination: the string to recieve
+ * @source: the string to add
+ *
+ * Return: adress of the final string
+ */
+char *_strcat(char *destination, char *source)
+{
+	char *new_string = NULL;
+	int len_dest = _strlen(destination);
+	int len_source = _strlen(source);
+
+	new_string = malloc(sizeof(*new_string) * (len_dest + len_source + 1));
+	_strcpy(destination, new_string);
+	_strcpy(source, new_string +len_dest);
+	new_string[len_dest + len_source] = '\0';
+	return (new_string);
+}
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @str1: string to search
+ * @str2: string to use
+ *
+ * Return: bytes in the initial segment of 5 that are part of accept
+ */
+int _strspn(char *str1, char *str2)
+{
+	int i = 0;
+	int match = 0;
+
+	while (str1[i] != '\0')
+	{
+		if (_strchr(str2, str1[i]) == NULL)
+			break;
+		match++;
+		i++;
+	}
+	return (match);
+}
+
+/**
+ * _strcspn - compute segment of str1 which consists of
+ * characters not in str2
+ * @str1: string to search
+ * @str2: string to be used
+ *
+ * Return: index at which a char in str1 exists in str2
+ */
+int _strcspn(char *str1, char *str2)
+{
+	int len = 0;
+	int i;
+
+	for (i = 0, str1[i] != '\0'; i++)
+	{
+		if (_strchr(str2, str1[i]) != NULL)
+			break;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _strchr - locates a char in a string
+ * @s: string to search
+ * @c: char to check
+ *
+ * Return: pointer to the first occurence of c in s
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] != c && s[i] != '\0'; i++)
+		;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
+}
